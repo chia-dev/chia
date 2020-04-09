00d3bdfd3072f7a8d8f6facb26361e73001e9608
chia-dev <63184255+chia-dev@users.noreply.github.com>
2020-04-10 07:21:55 +1000

    Updated to be compatibel with chia beta-1.0b3
    Handling of paths improved

--- generate_plots_yaml_file.py
+++ generate_plots_yaml_file.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 from blspy import PrivateKey, PublicKey
 
-from src.util.config import load_config, save_config
+from src.util.config import config_path_for_filename, load_config, save_config
+from src.util.default_root import DEFAULT_ROOT_PATH
 from src.util.path import make_path_relative, mkdir, path_from_root
 
 def main():
@@ -18,16 +19,21 @@ def main():
 
     python generate_plots_yaml_file.py -a True -d /mnt/bigdisk/plots     #will append entries to lots-generated.yaml, for 2nd, 3rd drives
     """
-    plot_config_filename = path_from_root() / "config" / "plots.yaml"
-    key_config_filename = path_from_root() / "config" / "keys.yaml"
+    root_path = DEFAULT_ROOT_PATH
+    plot_config_filename = config_path_for_filename(root_path, "plots.yaml")
+    key_config_filename = config_path_for_filename(root_path, "keys.yaml")
 
     parser = argparse.ArgumentParser(description="Chia plots.yaml generator")
 
     parser.add_argument("-a", "--append", help="Append to an existing output file", type=bool, default=False)
 
     new_plots_root = path_from_root(
-        load_config("config.yaml").get("harvester", {}).get("new_plot_root", "plots")
+        root_path,
+        load_config(root_path, "config.yaml")
+        .get("harvester", {})
+        .get("new_plot_root", "plots"),
     )
+
     parser.add_argument(
         "-d",
         "--final_dir",
@@ -42,7 +48,7 @@ def main():
         raise RuntimeError("Can not find keys.yaml.")
 
     # The seed is what will be used to generate a private key for each plot
-    key_config = load_config(key_config_filename)
+    key_config = load_config(root_path, key_config_filename)
     sk_seed: bytes = bytes.fromhex(key_config["sk_seed"])
 
     pool_pk: PublicKey
